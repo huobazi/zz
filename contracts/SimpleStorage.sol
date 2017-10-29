0156cb4dfa603823acf85826c31922a3186810b1
Marble Wu <huobazi@gmail.com>
2017-10-29 10:15:44 +0800

    write the SimpleStorage

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,1 +1,17 @@
+pragma solidity ^0.4.8
 
+contract SimpleStorage{
+    unit public storeData;
+
+    function SipleStorage(unit initVal){
+        storeData = initVal;
+    }
+
+    function set(unit m){
+       storeData = m;
+    }
+
+    function get() constant returns(unit retVal){
+       return storeData;
+    }
+}
