4c94ea53951b84c67ba799ebf789eb2feb43a02c
Marble Wu <huobazi@gmail.com>
2017-10-31 16:58:41 +0800

    change node settings

--- truffle.js
+++ truffle.js
@@ -1,6 +1,6 @@
 module.exports = {
     networks: {
-        node1: {
+        development: {
             host: "localhost",
             port: 22000, // was 8545,
             network_id: "*" // Match any network id
