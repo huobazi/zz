8d4fdd76af209c1a30217b5313bd445dd731f61b
Marble Wu <huobazi@gmail.com>
2017-10-31 15:13:56 +0800

    add greeter.sol

---
+++ contracts/Greeter.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.8;
+
+contract Greeter{
+    address creator;
+    string greeting;
+
+    function Greeter(string _greeting) public{
+        creator = msg.sender;
+        greeting = _greeting;
+
+    }
+
+    function greet() constant returns(string){
+        return greeting;
+    }
+
+    functionsetGreeting(string _newGreeting){
+        greeting = _newGreeting;
+    }
+
+    function kill(){
+        if(msg.sender == creator){
+            suicide(creator);
+        }
+    }
+
+
+}
