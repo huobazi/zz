a2d6d3811ca25c5c0554c7993ca58b9b6b56c6e4
Marble Wu <huobazi@gmail.com>
2017-10-31 11:33:54 +0800

    add multi-nodes

--- truffle.js
+++ truffle.js
@@ -1,9 +1,19 @@
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 22000, // was 8545,
-      network_id: "*" // Match any network id
+    networks: {
+        node1: {
+            host: "localhost",
+            port: 22000, // was 8545,
+            network_id: "*" // Match any network id
+        },
+        node4: {
+            host: "localhost",
+            port: 22003, // was 8545,
+            network_id: "*" // Match any network id
+        },
+        node7: {
+            host: "localhost",
+            port: 22006, // was 8545,
+            network_id: "*" // Match any network id
+        }
     }
-  }
 };
