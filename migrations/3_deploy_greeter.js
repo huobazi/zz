5fb3cb64e30f21038746b742be0cf02be97f1560
Marble Wu <huobazi@gmail.com>
2017-10-31 17:02:58 +0800

    update deploy

--- migrations/3_deploy_greeter.js
+++ migrations/3_deploy_greeter.js
@@ -1,5 +1,5 @@
 var Greeter = artifacts.require("Greeter");
 
 module.exports = function(deployer){
-    deployer.deploy(Greeter, "Hello", {privateFor:["ROAZBWtSacxXQrOe3FGAqJDyJjFePR5ce4TSIzmJ0Bc="]});
+    deployer.deploy(Greeter, "Hello Quorum!", {privateFor:["ROAZBWtSacxXQrOe3FGAqJDyJjFePR5ce4TSIzmJ0Bc="]});
 }
