ae7f55bdb862115b55b643079bc2d3c8fde7275a
Marble Wu <huobazi@gmail.com>
2017-11-08 17:39:30 +0800

    update config

--- truffle_example/truffle.js
+++ truffle_example/truffle.js
@@ -3,17 +3,17 @@ module.exports = {
         development: {
             host: "localhost",
             port: 22000,
-            network_id: "*" // Match any network id
+            network_id: "*", // Match any network id
         },
         node4: {
             host: "localhost",
             port: 22003, // as voter,
-            network_id: "*" // Match any network id
+            network_id: "*", // Match any network id
         },
         node7: {
             host: "localhost",
             port: 22006,
-            network_id: "*" // Match any network id
+            network_id: "*", // Match any network id
         }
     }
 };
